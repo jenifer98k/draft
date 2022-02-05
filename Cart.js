35dc28fae43a14a3b3fed4f929b2bbe9e909a48e
jenifer98k <85919892+jenifer98k@users.noreply.github.com>
2022-02-05 20:51:12 +0530

    v
    v

---
+++ Cart.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {useCart} from 'react-use-cart';
+const Cart= ()=>{
+    const {items}=useCart();
+    return(
+        <div>
+{console.warn()}
+        </div>
+    )
+}
+export default Cart;
